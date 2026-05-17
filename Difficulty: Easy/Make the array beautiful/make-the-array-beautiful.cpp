e70eab510759f335c4ae8571472d85541e1d74e7
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-05-17 21:50:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Make the array beautiful/make-the-array-beautiful.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    vector<int> makeBeautiful(vector<int> arr) {
+        vector<int> st;
+        
+        for(int x : arr) {
+            if(!st.empty() && 
+              ((st.back() >= 0 && x < 0) || (st.back() < 0 && x >= 0))) {
+                
+                st.pop_back();
+            }
+            else {
+                st.push_back(x);
+            }
+        }
+        
+        return st;
+    }
+};
