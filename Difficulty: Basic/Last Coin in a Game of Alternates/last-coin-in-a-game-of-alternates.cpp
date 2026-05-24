18de9a3f26aee32fad366dd3a5a8ceccd5480c14
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-05-24 21:12:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Last Coin in a Game of Alternates/last-coin-in-a-game-of-alternates.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int coin(vector<int>& arr) {
+        int left = 0, right = arr.size() - 1;
+
+        while (left < right) {
+            if (arr[left] >= arr[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return arr[left];
+    }
+};
