9ed696516cf9ee252aeb789f0638dfd8a5894d63
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-08-17 20:02:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Snake and Ladder Problem/snake-and-ladder-problem.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int minThrows(int n, vector<int>& lad, vector<int>& sn) {
+        int N = n * n;
+        vector<int> move(N + 1, -1);
+        for (int i = 0; i < lad.size(); i += 2)
+            move[lad[i]] = lad[i + 1];
+        for (int i = 0; i < sn.size(); i += 2)
+            move[sn[i]] = sn[i + 1];
+        vector<int> dist(N + 1, -1);
+        queue<int> q;
+        q.push(1);
+        dist[1] = 0;
+        while (!q.empty()) {
+            int curr = q.front();
+            q.pop();
+            if (curr == N)
+                return dist[curr];
+            for (int dice = 1; dice <= 6; dice++) {
+                int next = curr + dice;
+                if (next > N)
+                    break;
+                if (move[next] != -1)
+                    next = move[next];
+                if (dist[next] == -1) {
+                    dist[next] = dist[curr] + 1;
+                    q.push(next);
+                }
+            }
+        }
+        return -1;
+    }
+};
